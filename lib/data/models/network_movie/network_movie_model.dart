7327a5e11adf2fefab5177bc8aa60a32b82448bf
A'zambek <azambekergashev1@gmail.com>
2022-10-22 09:00:21 +0500

    network_movie model has created

---
+++ lib/data/models/network_movie/network_movie_model.dart
@@ -0,0 +1,11 @@
+class NetworkMovieModel {
+  NetworkMovieModel({
+    required this.imageUrl,
+    required this.name,
+    required this.videoUrl,
+  });
+
+  final String videoUrl;
+  final String imageUrl;
+  final String name;
+}
