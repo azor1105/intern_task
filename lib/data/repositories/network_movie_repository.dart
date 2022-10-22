e3111958cda680fc136595e3e2587e379332b73d
A'zambek <azambekergashev1@gmail.com>
2022-10-22 09:09:22 +0500

    network_movie repository is completed

--- lib/data/repositories/network_movie_repository.dart
+++ lib/data/repositories/network_movie_repository.dart
@@ -1,4 +1,10 @@
+import 'package:intern_task/data/models/network_movie/network_movie_model.dart';
+
 class NetworkMovieRepository {
+  static final NetworkMovieRepository _instance = NetworkMovieRepository.init();
+  static NetworkMovieRepository get instance => _instance;
+  NetworkMovieRepository.init();
+
   final imagesOfMovies = [
     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
@@ -7,7 +13,6 @@ class NetworkMovieRepository {
     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerFun.jpg",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerJoyrides.jpg",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg",
-    "https://storage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/SubaruOutbackOnStreetAndDirt.jpg",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg"
   ];
@@ -20,8 +25,33 @@ class NetworkMovieRepository {
     "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
-    "https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.jpg",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
   ];
+
+  final nameOfMovies = [
+    "Big buck bunny",
+    "Elephant's dream",
+    "For bigger blazes",
+    "For bigger escapes",
+    "For bigger fun",
+    "For bigger joyrides",
+    "For bigger meltdowns",
+    "Subaru outback on street and dirt",
+    "Tears of steel",
+  ];
+
+  List<NetworkMovieModel> networkMovies = [];
+
+  void getNetworkMovies() {
+    for (int i = 0; i < urlsOfMovies.length; i++) {
+      networkMovies.add(
+        NetworkMovieModel(
+          imageUrl: imagesOfMovies[i],
+          name: nameOfMovies[i],
+          videoUrl: urlsOfMovies[i],
+        ),
+      );
+    }
+  }
 }
